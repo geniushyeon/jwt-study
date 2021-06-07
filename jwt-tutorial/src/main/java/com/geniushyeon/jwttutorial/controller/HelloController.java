01cfd2fb8fa4f013f3d9ba9082d53f9d339e4df5
geniushyeon <jhlee0912@icloud.com>
2021-06-07 23:37:48 +0900

    🎉 project start

---
+++ jwt-tutorial/src/main/java/com/geniushyeon/jwttutorial/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.geniushyeon.jwttutorial.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+  @GetMapping("/hello")
+  public ResponseEntity<String> hello() {
+    return ResponseEntity.ok("hello");
+  }
+
+}
